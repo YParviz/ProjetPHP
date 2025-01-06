f3207b935310b99cd3b953b2610e83eed01edbd2
YParviz <113101521+YParviz@users.noreply.github.com>
2025-01-07 00:15:43 +0100

    Update requetes_youssef.sql

--- DB/requetes_youssef.sql
+++ DB/requetes_youssef.sql
@@ -26,7 +26,7 @@ WHERE U.id_utilisateur = 'id_utilisateur';
 --Historique de ses participations ( ses arguments)
 
 --Si on veut tout l'historique : 
-SELECT A.id_arg, A.date_poste, A.texte, D.id_debat, D.nom_d, D.date_creation, C.nom_c
+SELECT A.id_arg, A.date_poste, A.texte, D.id_debat, D.nom_d, D.date_creation, C.nom_camp
 FROM Utilisateur U
 JOIN Argument A ON U.id_utilisateur = A.id_utilisateur
 JOIN Camp C ON A.id_camp = C.id_camp
@@ -49,7 +49,7 @@ FROM Utilisateur U
 JOIN Argument A ON U.id_utilisateur = A.id_utilisateur
 JOIN Camp C ON A.id_camp = C.id_camp
 JOIN Debat D ON C.id_debat = D.id_debat
-JOIN Statistiques S ON D.id_debat = S.id_debat
+JOIN Statistique S ON D.id_debat = S.id_debat
 WHERE U.id_utilisateur = 5
 AND C.id_camp = S.id_camp_gagnant
 AND NOT EXISTS (
