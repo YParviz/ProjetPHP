4a3a1df3dd104b398b537770ea124613fe1c1981
Thomas HAY <159462477+Faynard@users.noreply.github.com>
2025-01-07 10:08:15 +0100

    Update creation_debat.php

--- PHP/creation_debat.php
+++ PHP/creation_debat.php
@@ -1,9 +1,8 @@
 <?php
-$host = "localhost";
-$host_fac = "postgresql1.ensinfo.sciences.univ-nantes.prive";   
-$dbname = "debatarena";
-$user = "thay";
-$password = "thay";
+$host = "postgresql1.ensinfo.sciences.univ-nantes.prive";
+$dbname = "l3_alt_02";
+$user = "l3_alt_02";
+$password = "l3_alt_02";
 try {
     // Connexion à PostgreSQL
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
