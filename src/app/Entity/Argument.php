ae96f50fcc1af276277a7a7672521700d250c198
Firmin EON <firmin.eon@etu.univ-nantes.fr>
2025-03-14 11:39:01 +0100

    Ajout de l'entité argument

---
+++ src/app/Entity/Argument.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Entity;
+
+class Argument {
+    private int $id;
+    private string $texte;
+    private int $idCamp;
+    private int $idArgPrincipal;
+    private int $idUtilisateur;
+    private string $datePoste;
+
+    public function __construct(int $id, string $texte, int $idCamp, ?int $idArgPrincipal, int $idUtilisateur, string $datePoste) {
+        $this->id = $id;
+        $this->texte = $texte;
+        $this->idCamp = $idCamp;
+        $this->idArgPrincipal = $idArgPrincipal;
+        $this->idUtilisateur = $idUtilisateur;
+        $this->datePoste = $datePoste;
+    }
+
+    public function getId(): int { return $this->id; }
+    public function setId(int $id): void { $this->id = $id; }
+
+    public function getTexte(): string { return $this->texte; }
+    public function setTexte(string $texte): void { $this->texte = $texte; }
+
+    public function getIdCamp(): int { return $this->idCamp; }
+    public function setIdCamp(int $idCamp): void { $this->idCamp = $idCamp; }
+
+    public function getIdArgPrincipal(): ?int { return $this->idArgPrincipal; }
+    public function setIdArgPrincipal(?int $idArgPrincipal): void { $this->idArgPrincipal = $idArgPrincipal; }
+
+    public function getIdUtilisateur(): int { return $this->idUtilisateur; }
+    public function setIdUtilisateur(int $idUtilisateur): void { $this->idUtilisateur = $idUtilisateur; }
+
+    public function getDatePoste(): string { return $this->datePoste; }
+    public function setDatePoste(string $datePoste): void { $this->datePoste = $datePoste; }
+}
