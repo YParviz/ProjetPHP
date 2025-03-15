e9afb3689c1dfb17784ed18966b13a350a8eb983
Firmin EON <firmin.eon@etu.univ-nantes.fr>
2025-03-15 12:00:41 +0100

    Argument entity

--- src/app/Entity/Argument.php
+++ src/app/Entity/Argument.php
@@ -2,30 +2,28 @@
 
 namespace Entity;
 
-use DateTime;
-
 class Argument {
     private int $id;
-    private string $texte;
+    private string $text;
     private int $idCamp;
     private ?int $idArgPrincipal;
-    private int $idUtilisateur;
-    private string $datePoste;
+    private int $userId;
+    private string $datePosted;
 
-    public function __construct(int $id, string $texte, int $idCamp, ?int $idArgPrincipal, int $idUtilisateur, string $datePoste) {
+    public function __construct(int $id, string $text, int $idCamp, ?int $idArgPrincipal, int $userId, string $datePosted) {
         $this->id = $id;
-        $this->texte = $texte;
+        $this->text = $text;
         $this->idCamp = $idCamp;
         $this->idArgPrincipal = $idArgPrincipal;
-        $this->idUtilisateur = $idUtilisateur;
-        $this->datePoste = $datePoste;
+        $this->userId = $userId;
+        $this->datePosted = $datePosted;
     }
 
     public function getId(): int { return $this->id; }
     public function setId(int $id): void { $this->id = $id; }
 
-    public function getTexte(): string { return $this->texte; }
-    public function setTexte(string $texte): void { $this->texte = $texte; }
+    public function getText(): string { return $this->text; }
+    public function setText(string $text): void { $this->text = $text; }
 
     public function getIdCamp(): int { return $this->idCamp; }
     public function setIdCamp(int $idCamp): void { $this->idCamp = $idCamp; }
@@ -33,9 +31,9 @@ class Argument {
     public function getIdArgPrincipal(): ?int { return $this->idArgPrincipal; }
     public function setIdArgPrincipal(?int $idArgPrincipal): void { $this->idArgPrincipal = $idArgPrincipal; }
 
-    public function getIdUtilisateur(): int { return $this->idUtilisateur; }
-    public function setIdUtilisateur(int $idUtilisateur): void { $this->idUtilisateur = $idUtilisateur; }
+    public function getUserId(): int { return $this->userId; }
+    public function setUserId(int $userID): void { $this->userId = $userID; }
 
-    public function getDatePoste(): DateTime { return $this->datePoste; }
-    public function setDatePoste(DateTime $datePoste): void { $this->datePoste = $datePoste; }
+    public function getDatePosted(): string { return $this->datePosted; }
+    public function setDatePosted(string $datePosted): void { $this->datePosted = $datePosted; }
 }
