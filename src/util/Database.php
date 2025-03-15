a8fb97f13a8aedb7229f04b6ef243bbc3b713f27
Firmin EON <firmin.eon@etu.univ-nantes.fr>
2025-03-15 10:51:20 +0100

    Ajout utilitaire pour la connexion de la DB

---
+++ src/util/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Util;
+
+use PDO;
+
+class Database
+{
+    public static function connect(): PDO
+    {
+        $host = $_ENV['DB_HOST'];
+        $name = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASSWORD'];
+        $port = $_ENV['DB_PORT'];
+        $charset = $_ENV['DB_CHARSET'];
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$name;charset=$charset", $user, $pass);
+        return $pdo;
+    }
+}
