b56f08238b6c1d6f1f979a8e913c38029d3474fd
Firmin EON <firmin.eon@etu.univ-nantes.fr>
2025-04-01 21:44:11 +0200

    Modif de style poster un argument

--- src/app/Views/Arguments/create.php
+++ src/app/Views/Arguments/create.php
@@ -1,11 +1,20 @@
-<h1>Poster un argument</h1>
-<form method="post" action="/debate/<?= $idDebate ?>/postArg">
-    <input type="radio" name="camp" id="camp1" value="<?= $camps[0]->getId() ?>" checked/>
-    <label for="camp1"><?= $camps[0]->getName() ?></label>
-    <input type="radio" name="camp" id="camp2" value="<?= $camps[1]->getId() ?>" />
-    <label for="camp2"><?= $camps[1]->getName() ?></label>
-    <br>
-    <textarea name="argument"></textarea>
-    <br>
-    <input type="submit">
-</form>
+<head>
+    <link rel="stylesheet" href="/css/arguments/styleCreateArgument.css">
+</head>
+<body>
+<div class="container">
+    <form class="card" method="post" action="/debate/<?= $idDebate ?>/postArg">
+        <h1>Poster un argument</h1>
+        <p>Camp :
+        <input class="radioSetCamp" type="radio" name="camp" id="camp1" value="<?= $camps[0]->getId() ?>" checked/>
+        <label for="camp1"><?= $camps[0]->getName() ?></label>
+        <input class="radioSetCamp" type="radio" name="camp" id="camp2" value="<?= $camps[1]->getId() ?>" />
+        <label for="camp2"><?= $camps[1]->getName() ?></label>
+        </p>
+        <textarea class="textArgument" name="argument" required></textarea>
+        <br>
+        <button class="buttonBack" onclick="window.location.href='/debat/<?= $idDebate ?>'">Retour</button>
+        <input class="buttonSubmit" id="buttonBack" type="submit">
+    </form>
+</div>
+</body>
